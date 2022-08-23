c6e4a6477ec35a274f1f7c5db5f99ad79f5e6b1d
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2022-08-23 10:06:11 +0530

    Socket connection and chart change on coin change

--- PaperTrading/script.js
+++ PaperTrading/script.js
@@ -1,4 +1,4 @@
-const dataPoints = [];
+let dataPoints = [];
 let lastItem;
 let chart;
 let previousPrice = 0;
@@ -7,6 +7,12 @@ const coinPairs = [];
 let coinPairUpper = 'BTCUSDT'
 let coinPairLower = 'btcusdt'
 
+let coin_pairs_url = 'https://api.binance.com/api/v1/exchangeInfo';
+let api_url = `https://api.binance.com/api/v3/klines?symbol=${coinPairUpper}&interval=1m&limit=120`;
+let webSocket_url = `wss://stream.binance.com:9443/ws/${coinPairLower}@kline_1m`
+
+
+const coinTicker = document.querySelectorAll('.cointicker');
 const coinSearch = document.querySelector('.search-form');
 const tickerPrice = document.querySelectorAll('.ticker-price');
 const searchInput = document.querySelector('.coin-search');
@@ -14,7 +20,6 @@ const suggestions = document.querySelector('.suggestions');
 let suggestionList;
 
 
-const coin_pairs_url = 'https://api.binance.com/api/v1/exchangeInfo';
 async function getCoins() {
     const response = await fetch(coin_pairs_url);
     const data = await response.json();
@@ -53,9 +58,26 @@ function displayMatches() {
 
 }
 
+async function generateChartData() {
+    // dataPoints = [];
+    const response = await fetch(api_url);
+    const data = await response.json();
+    // console.log(data);
+    data.forEach(e => {
+        dataPoints.push({
+            x: new Date(e[0]),
+            y: [
+                parseFloat(e[1]), parseFloat(e[2]), parseFloat(e[3]), parseFloat(e[4])
+            ]
+        })
+    });
+    chart.render();
+    lastItem = dataPoints[dataPoints.length - 1];
+}
 
 
-window.onload = function () {
+
+function drawChart() {
 
     chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
@@ -105,74 +127,91 @@ window.onload = function () {
         redirect: 'follow'
     };
     // const api_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1';
-    const api_url = `https://api.binance.com/api/v3/klines?symbol=${coinPairUpper}&interval=1m&limit=120`;
-    async function generateChartData() {
-        let chartData = [];
-        const response = await fetch(api_url);
-        const data = await response.json();
-        // console.log(data);
-        data.forEach(e => {
-            dataPoints.push({
-                x: new Date(e[0]),
-                y: [
-                    parseFloat(e[1]), parseFloat(e[2]), parseFloat(e[3]), parseFloat(e[4])
-                ]
-            })
-        });
-        chart.render();
-        lastItem = dataPoints[dataPoints.length - 1];
-    }
+    // async function generateChartData() {
+    //     // dataPoints = [];
+    //     const response = await fetch(api_url);
+    //     const data = await response.json();
+    //     // console.log(data);
+    //     data.forEach(e => {
+    //         dataPoints.push({
+    //             x: new Date(e[0]),
+    //             y: [
+    //                 parseFloat(e[1]), parseFloat(e[2]), parseFloat(e[3]), parseFloat(e[4])
+    //             ]
+    //         })
+    //     });
+    //     chart.render();
+    //     lastItem = dataPoints[dataPoints.length - 1];
+    // }
 
-    getCoins();
-    generateChartData();
+
+    // generateChartData();
     /////////////////////////////////////////////////
 }
 
-let ws = new WebSocket(`wss://stream.binance.com:9443/ws/${coinPairLower}@kline_1m`);
-let oldtime = 1;
-let flag = true;
-ws.onmessage = (e) => {
-    if (flag) {
-        oldtime = lastItem.x.getTime();
-        flag = false;
+let connectionExist = false;
+let ws = new WebSocket(webSocket_url);
+function startSocketConnection() {
+    if (connectionExist) {
+        ws.close();
+        ws = new WebSocket(webSocket_url);
     }
-    let priceObj = JSON.parse(e.data);
-    let newtime = priceObj.k.t;
-    if (oldtime == newtime) {
-        // console.log(oldtime, "match");
-        dataPoints.pop();
-    }
-    previousPrice < parseFloat(priceObj.k.c) ? color = 'green' : color = 'red';
-    tickerPrice.forEach(e => {
-        e.innerHTML = parseFloat(priceObj.k.c);
-        e.style.color = color;
-    })
-    previousPrice = parseFloat(priceObj.k.c);
-    dataPoints.push({
-        x: new Date(priceObj.k.t),
-        y: [
-            parseFloat(priceObj.k.o), parseFloat(priceObj.k.h), parseFloat(priceObj.k.l), parseFloat(priceObj.k.c)
-        ]
-    })
-    oldtime = newtime;
-    chart.render();
-};
-
-
-// function testing() {
-//     console.log(this);
-// }
+    let oldtime = 1;
+    let flag = true;
+    ws.onmessage = (e) => {
+        if (flag) {
+            oldtime = lastItem.x.getTime();
+            flag = false;
+        }
+        let priceObj = JSON.parse(e.data);
+        let newtime = priceObj.k.t;
+        if (oldtime == newtime) {
+            // console.log(oldtime, "match");
+            dataPoints.pop();
+        }
+        previousPrice < parseFloat(priceObj.k.c) ? color = 'green' : color = 'red';
+        tickerPrice.forEach(e => {
+            e.innerHTML = parseFloat(priceObj.k.c);
+            e.style.color = color;
+        })
+        previousPrice = parseFloat(priceObj.k.c);
+        dataPoints.push({
+            x: new Date(priceObj.k.t),
+            y: [
+                parseFloat(priceObj.k.o), parseFloat(priceObj.k.h), parseFloat(priceObj.k.l), parseFloat(priceObj.k.c)
+            ]
+        })
+        oldtime = newtime;
+        chart.render();
+    };
+    connectionExist = true;
+}
 
 function readForm(e) {
     e.preventDefault();
     const searchedCoin = this.querySelector('[name=coin-search]').value;
     changeCoin(searchedCoin);
+    this.reset();
 }
 
-function changeCoin(coinName){
+function changeCoin(coinName) {
     coinPairUpper = coinName.toUpperCase();
     coinPairLower = coinName.toLowerCase();
+    coinTicker.forEach(e => {
+        e.innerHTML = coinPairUpper;
+    });
+    api_url = `https://api.binance.com/api/v3/klines?symbol=${coinPairUpper}&interval=1m&limit=120`;
+    webSocket_url = `wss://stream.binance.com:9443/ws/${coinPairLower}@kline_1m`;
+    dataPoints = [];
+    generateChartData();
+    drawChart();
+    startSocketConnection();
 }
 
 searchInput.addEventListener('input', displayMatches);
 coinSearch.addEventListener('submit', readForm);
+
+generateChartData();
+drawChart();
+startSocketConnection();
+getCoins();
