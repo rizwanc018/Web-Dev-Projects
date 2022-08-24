7c9d9a17e66f6c9551a6c5f32c77ccea87384541
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2022-08-24 15:19:10 +0530

    implimented buy and sell

--- PaperTrading/script.js
+++ PaperTrading/script.js
@@ -6,18 +6,34 @@ let color = 'black';
 const coinPairs = [];
 let coinPairUpper = 'BTCUSDT'
 let coinPairLower = 'btcusdt'
+let currentPair = coinPairUpper;
+let balance = 1000;
+let closePrice;  // close price from ws
+let buyLimit = -100000000;
+let sellLimit = 100000000;
+let costOfBuying; //
+let costofSelling // money made from selling
+let currentCoinBalance;
+let newlyBoughtQuantity;
+let newlySoldQuantity;
 
 let coin_pairs_url = 'https://api.binance.com/api/v1/exchangeInfo';
 let api_url = `https://api.binance.com/api/v3/klines?symbol=${coinPairUpper}&interval=1m&limit=120`;
 let webSocket_url = `wss://stream.binance.com:9443/ws/${coinPairLower}@kline_1m`
 
 
+const fundForm = document.querySelector('.fund-form');
+const balanceElt = document.querySelector('.balance-amount');
+const buyForm = document.querySelector('.buy-form');
+const sellForm = document.querySelector('.sell-form');
 const coinTicker = document.querySelectorAll('.cointicker');
 const coinSearch = document.querySelector('.search-form');
+const portfolio_ul = document.querySelector('.coins-ul');
 const tickerPrice = document.querySelectorAll('.ticker-price');
 const baseVolume = document.querySelector('.base-vol');
 const quoteVolume = document.querySelector('.quote-vol');
 const trades = document.querySelector('.trades');
+
 
 const searchInput = document.querySelector('.coin-search');
 const suggestions = document.querySelector('.suggestions');
@@ -47,9 +63,9 @@ function displayMatches() {
         suggestions.innerHTML = null;
         return;
     }
-    console.log(this.value);
+    // console.log(this.value);
     const matchArray = findMatches(this.value, coinPairs);
-    const html = matchArray.map(pairs => {
+    let html = matchArray.map(pairs => {
         return `
           <li class="suggestionlist">
             <span class="suggestionlist">${pairs}</span>
@@ -150,7 +166,20 @@ function startSocketConnection() {
             // console.log(oldtime, "match");
             dataPoints.pop();
         }
-        previousPrice < parseFloat(priceObj.k.c) ? color = 'green' : color = 'red';
+        closePrice = parseFloat(priceObj.k.c);
+
+        if(closePrice <= buyLimit) {
+            updateBuy();
+            buyLimit = -1000000000;
+        }
+
+        if(closePrice >= sellLimit) {
+            // console.log('inside ws');
+            updateSell();
+            sellLimit = 1000000000;
+        }
+
+        previousPrice < closePrice ? color = 'green' : color = 'red';
         tickerPrice.forEach(e => {
             e.innerHTML = parseFloat(priceObj.k.c);
             e.style.color = color;
@@ -158,11 +187,12 @@ function startSocketConnection() {
         baseVolume.innerHTML = parseFloat(priceObj.k.v).toFixed(2);
         quoteVolume.innerHTML = parseFloat(priceObj.k.q).toFixed(2);
         trades.innerHTML = parseFloat(priceObj.k.n).toFixed(2);
-        previousPrice = parseFloat(priceObj.k.c);
+        previousPrice = closePrice;
+
         dataPoints.push({
             x: new Date(priceObj.k.t),
             y: [
-                parseFloat(priceObj.k.o), parseFloat(priceObj.k.h), parseFloat(priceObj.k.l), parseFloat(priceObj.k.c)
+                parseFloat(priceObj.k.o), parseFloat(priceObj.k.h), parseFloat(priceObj.k.l), closePrice
             ]
         })
         oldtime = newtime;
@@ -185,8 +215,10 @@ function readForm(e) {
 }
 
 function changeCoin(coinName) {
+    buyLimit = -100000000;
     coinPairUpper = coinName.toUpperCase();
     coinPairLower = coinName.toLowerCase();
+    currentPair = coinPairUpper;
     coinTicker.forEach(e => {
         e.innerHTML = coinPairUpper;
     });
@@ -197,12 +229,105 @@ function changeCoin(coinName) {
     generateChartData();
     drawChart();
     startSocketConnection();
+
+}
+
+function updateBalance() {
+    balanceElt.innerHTML = balance.toFixed(3);
+}
+
+function addFund(e) {
+    e.preventDefault();
+    const amountToAdd = this.querySelector('[name=add-funds]').value;
+    balance += parseFloat(amountToAdd);
+    updateBalance();
+    this.reset();
+}
+
+function buyCoin(e) {
+    e.preventDefault();
+    let price;
+    let quantity;
+    price = parseFloat(this.querySelector('#buy-price').value);
+    quantity = parseFloat(this.querySelector('#buy-quantity').value);
+    costOfBuying = price * quantity;
+    if (balance < costOfBuying) {
+        alert(`Not enough balance`);
+        return;
+    }
+    buyLimit = price;
+    newlyBoughtQuantity = quantity;
+    this.reset();
+}
+
+function sellCoin(e) {
+    e.preventDefault();
+    let sellPrice;
+    let sellQuantity;
+    sellPrice = parseFloat(this.querySelector('#sell-price').value);
+    sellQuantity = parseFloat(this.querySelector('#sell-quantity').value);
+    let coinExist = document.querySelector(`#${currentPair}`);
+    if(!coinExist) {
+        // console.log('Not enough coin');
+        alert('Not enough coin');
+        return;
+    }
+    currentCoinBalance = parseFloat(document.querySelector(`#${currentPair}-count`).innerHTML); 
+    if (currentCoinBalance < sellQuantity) {
+        // console.log('Not enough coin');
+        alert('Not enough coin');
+        return;
+    }
+    sellLimit = sellPrice;
+    costofSelling = sellPrice * sellQuantity;
+    newlySoldQuantity = sellQuantity;
+    this.reset();
+}
+
+function updatePortfolioAfterBuying() {
+    let coinExist = document.querySelector(`#${currentPair}`);
+    if (coinExist) {
+        currentCoinBalance = parseFloat(document.querySelector(`#${currentPair}-count`).innerHTML); 
+        // console.log(currentCoinBalance + newlyBoughtQuantity);
+        document.querySelector(`#${currentPair}-count`).innerHTML = currentCoinBalance + newlyBoughtQuantity;
+    } else {
+        currentCoinBalance = newlyBoughtQuantity;
+        let html = `<li> 
+                    <span id="${currentPair}">${currentPair}</span>
+                    <span id="${currentPair}-count">${currentCoinBalance}</span>
+                </li>`;
+        portfolio_ul.innerHTML += html;
+    }
+
+}
+
+function updatePortfolioAfterSelling() {
+    currentCoinBalance = parseFloat(document.querySelector(`#${currentPair}-count`).innerHTML); 
+    // console.log(currentCoinBalance);
+    document.querySelector(`#${currentPair}-count`).innerHTML = currentCoinBalance - newlySoldQuantity;
+}
+
+function updateBuy() {
+    balance -= costOfBuying;
+    updateBalance();
+    updatePortfolioAfterBuying();
+}
+
+function updateSell() {
+    balance += costofSelling;
+    // console.log('here balance', balance)
+    updateBalance();
+    updatePortfolioAfterSelling();
 }
 
 generateChartData();
 drawChart();
 startSocketConnection();
 getCoins();
+updateBalance();
 
 searchInput.addEventListener('input', displayMatches);
 coinSearch.addEventListener('submit', readForm);
+fundForm.addEventListener('submit', addFund);
+buyForm.addEventListener('submit', buyCoin);
+sellForm.addEventListener('submit',sellCoin);
